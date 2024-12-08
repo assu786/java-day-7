82dfd5ce647d73faba7421cba2e41780b5d1de8a
assu786 <shaikaqsa.78@gmail.com>
2024-12-08 16:16:15 +0530

    Add files via upload

---
+++ prefixsum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class v
+{
+    public static void main(String args[])
+    {
+        int a[]={12,13,1,5,7};
+        int s=0;
+        for(int i=0;i<a.length;i++)
+        {
+            s+=a[i];
+            int res=s/(i+1);
+            a[i]=res;
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
